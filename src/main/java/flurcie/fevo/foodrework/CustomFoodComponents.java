c5602b9e035d5bb1ae425579505e472cd35b0f65
flurcie <flurcie@gmail.com>
2025-03-06 01:44:51 +0100

    foundations for food rework introduced.

---
+++ src/main/java/flurcie/fevo/foodrework/CustomFoodComponents.java
@@ -0,0 +1,171 @@
+package flurcie.fevo.foodrework;
+
+import net.minecraft.component.type.FoodComponent;
+
+public class CustomFoodComponents {
+    public static final FoodComponent APPLE = new FoodComponent.Builder()
+            .nutrition(20)
+            .saturationModifier(1.3F)
+            .build();
+    public static final FoodComponent BAKED_POTATO = new FoodComponent.Builder()
+            .nutrition(5)
+            .saturationModifier(0.6F)
+            .build();
+    public static final FoodComponent BEEF = new FoodComponent.Builder()
+            .nutrition(3)
+            .saturationModifier(0.3F)
+            .build();
+    public static final FoodComponent BEETROOT = new FoodComponent.Builder()
+            .nutrition(1)
+            .saturationModifier(0.6F)
+            .build();
+    public static final FoodComponent BEETROOT_SOUP = new FoodComponent.Builder()
+            .nutrition(6)
+            .saturationModifier(0.6F)
+            .build();
+    public static final FoodComponent BREAD = new FoodComponent.Builder()
+            .nutrition(5)
+            .saturationModifier(0.6F)
+            .build();
+    public static final FoodComponent CARROT = new FoodComponent.Builder()
+            .nutrition(3)
+            .saturationModifier(0.6F)
+            .build();
+    public static final FoodComponent CHICKEN = new FoodComponent.Builder()
+            .nutrition(2)
+            .saturationModifier(0.3F)
+            .build();
+    public static final FoodComponent CHORUS_FRUIT = new FoodComponent.Builder()
+            .nutrition(4)
+            .saturationModifier(0.3F)
+            .alwaysEdible()
+            .build();
+    public static final FoodComponent COD = new FoodComponent.Builder()
+            .nutrition(2)
+            .saturationModifier(0.1F)
+            .build();
+    public static final FoodComponent COOKED_BEEF = new FoodComponent.Builder()
+            .nutrition(8)
+            .saturationModifier(0.8F)
+            .build();
+    public static final FoodComponent COOKED_CHICKEN = new FoodComponent.Builder()
+            .nutrition(6)
+            .saturationModifier(0.6F)
+            .build();
+    public static final FoodComponent COOKED_COD = new FoodComponent.Builder()
+            .nutrition(5)
+            .saturationModifier(0.6F)
+            .build();
+    public static final FoodComponent COOKED_MUTTON = new FoodComponent.Builder()
+            .nutrition(6)
+            .saturationModifier(0.8F)
+            .build();
+    public static final FoodComponent COOKED_PORKCHOP = new FoodComponent.Builder()
+            .nutrition(8)
+            .saturationModifier(0.8F)
+            .build();
+    public static final FoodComponent COOKED_RABBIT = new FoodComponent.Builder()
+            .nutrition(5)
+            .saturationModifier(0.6F)
+            .build();
+    public static final FoodComponent COOKED_SALMON = new FoodComponent.Builder()
+            .nutrition(6)
+            .saturationModifier(0.8F)
+            .build();
+    public static final FoodComponent COOKIE = new FoodComponent.Builder()
+            .nutrition(2)
+            .saturationModifier(0.1F)
+            .build();
+    public static final FoodComponent DRIED_KELP = new FoodComponent.Builder()
+            .nutrition(1)
+            .saturationModifier(0.3F)
+            .build();
+    public static final FoodComponent ENCHANTED_GOLDEN_APPLE = new FoodComponent.Builder()
+            .nutrition(4)
+            .saturationModifier(1.2F)
+            .alwaysEdible()
+            .build();
+    public static final FoodComponent GOLDEN_APPLE = new FoodComponent.Builder()
+            .nutrition(4)
+            .saturationModifier(1.2F)
+            .alwaysEdible()
+            .build();
+    public static final FoodComponent GOLDEN_CARROT = new FoodComponent.Builder()
+            .nutrition(6)
+            .saturationModifier(1.2F)
+            .build();
+    public static final FoodComponent HONEY_BOTTLE = new FoodComponent.Builder()
+            .nutrition(6)
+            .saturationModifier(0.1F)
+            .alwaysEdible()
+            .build();
+    public static final FoodComponent MELON_SLICE = new FoodComponent.Builder()
+            .nutrition(2)
+            .saturationModifier(0.3F)
+            .build();
+    public static final FoodComponent MUSHROOM_STEW = new FoodComponent.Builder()
+            .nutrition(6)
+            .saturationModifier(0.6F)
+            .build();
+    public static final FoodComponent MUTTON = new FoodComponent.Builder()
+            .nutrition(2)
+            .saturationModifier(0.3F)
+            .build();
+    public static final FoodComponent POISONOUS_POTATO = new FoodComponent.Builder()
+            .nutrition(2)
+            .saturationModifier(0.3F)
+            .build();
+    public static final FoodComponent PORKCHOP = new FoodComponent.Builder()
+            .nutrition(3)
+            .saturationModifier(0.3F)
+            .build();
+    public static final FoodComponent POTATO = new FoodComponent.Builder()
+            .nutrition(1)
+            .saturationModifier(0.3F)
+            .build();
+    public static final FoodComponent PUFFERFISH = new FoodComponent.Builder()
+            .nutrition(1)
+            .saturationModifier(0.1F)
+            .build();
+    public static final FoodComponent PUMPKIN_PIE = new FoodComponent.Builder()
+            .nutrition(8)
+            .saturationModifier(0.3F)
+            .build();
+    public static final FoodComponent RABBIT = new FoodComponent.Builder()
+            .nutrition(3)
+            .saturationModifier(0.3F)
+            .build();
+    public static final FoodComponent RABBIT_STEW = new FoodComponent.Builder()
+            .nutrition(10)
+            .saturationModifier(0.6F)
+            .build();
+    public static final FoodComponent ROTTEN_FLESH = new FoodComponent.Builder()
+            .nutrition(4)
+            .saturationModifier(0.1F)
+            .build();
+    public static final FoodComponent SALMON = new FoodComponent.Builder()
+            .nutrition(2)
+            .saturationModifier(0.1F)
+            .build();
+    public static final FoodComponent SPIDER_EYE = new FoodComponent.Builder()
+            .nutrition(2)
+            .saturationModifier(0.8F)
+            .build();
+    public static final FoodComponent SUSPICIOUS_STEW = new FoodComponent.Builder()
+            .nutrition(6)
+            .saturationModifier(0.6F)
+            .alwaysEdible()
+            .build();
+    public static final FoodComponent SWEET_BERRIES = new FoodComponent.Builder()
+            .nutrition(2)
+            .saturationModifier(0.1F)
+            .build();
+    public static final FoodComponent GLOW_BERRIES = new FoodComponent.Builder()
+            .nutrition(2)
+            .saturationModifier(0.1F)
+            .build();
+    public static final FoodComponent TROPICAL_FISH = new FoodComponent.Builder()
+            .nutrition(1)
+            .saturationModifier(0.1F)
+            .build();
+}
--- src/main/java/flurcie/fevo/mixin/FoodItemsMixin.java
+++ src/main/java/flurcie/fevo/mixin/FoodItemsMixin.java
@@ -1,7 +1,11 @@
 package flurcie.fevo.mixin;
 
+import flurcie.fevo.foodrework.CustomConsumableComponents;
+import flurcie.fevo.foodrework.CustomFoodComponents;
 import net.minecraft.component.DataComponentTypes;
-import net.minecraft.component.type.*;
+import net.minecraft.component.type.ConsumableComponents;
+import net.minecraft.component.type.OminousBottleAmplifierComponent;
+import net.minecraft.component.type.SuspiciousStewEffectsComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.util.Rarity;
@@ -18,233 +22,203 @@ public abstract class FoodItemsMixin {
   private static Item modifyFoodItems(String id, Item.Settings settings) {
     if ("apple".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.APPLE)
-              // .food(FoodComponents.APPLE, ConsumableComponents.APPLE)
+              .food(CustomFoodComponents.APPLE, CustomConsumableComponents.APPLE)
               .maxCount(16));
     }
     if ("bread".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.BREAD)
-              // .food(FoodComponents.BREAD, ConsumableComponents.BREAD)
+              .food(CustomFoodComponents.BREAD, CustomConsumableComponents.BREAD)
               .maxCount(16));
     }
     if ("porkchop".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.PORKCHOP)
-              // .food(FoodComponents.PORKCHOP, ConsumableComponents.PORKCHOP)
+              .food(CustomFoodComponents.PORKCHOP, CustomConsumableComponents.PORKCHOP)
               .maxCount(16));
     }
     if ("cooked_porkchop".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.COOKED_PORKCHOP)
-              // .food(FoodComponents.COOKED_PORKCHOP, ConsumableComponents.COOKED_PORKCHOP)
+              .food(CustomFoodComponents.COOKED_PORKCHOP, CustomConsumableComponents.COOKED_PORKCHOP)
               .maxCount(16));
     }
     if ("golden_apple".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.GOLDEN_APPLE, ConsumableComponents.GOLDEN_APPLE)
+              .food(CustomFoodComponents.GOLDEN_APPLE, CustomConsumableComponents.GOLDEN_APPLE)
               .maxCount(16));
     }
     if ("enchanted_golden_apple".equals(id)) {
       return Items.register(id, new Item.Settings()
               .rarity(Rarity.RARE)
-              .food(FoodComponents.ENCHANTED_GOLDEN_APPLE, ConsumableComponents.ENCHANTED_GOLDEN_APPLE)
+              .food(CustomFoodComponents.ENCHANTED_GOLDEN_APPLE, CustomConsumableComponents.ENCHANTED_GOLDEN_APPLE)
               .component(DataComponentTypes.ENCHANTMENT_GLINT_OVERRIDE, true)
               .maxCount(16));
     }
     if ("mushroom_stew".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.MUSHROOM_STEW)
-              // .food(FoodComponents.MUSHROOM_STEW, ConsumableComponents.MUSHROOM_STEW)
+              .food(CustomFoodComponents.MUSHROOM_STEW, CustomConsumableComponents.MUSHROOM_STEW)
               .useRemainder(Items.BOWL)
               .maxCount(16));
     }
     if ("cod".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.COD)
-              // .food(FoodComponents.COD, ConsumableComponents.COD)
+              .food(CustomFoodComponents.COD, CustomConsumableComponents.COD)
               .maxCount(16));
     }
     if ("salmon".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.SALMON)
-              // .food(FoodComponents.SALMON, ConsumableComponents.SALMON)
+              .food(CustomFoodComponents.SALMON, CustomConsumableComponents.SALMON)
               .maxCount(16));
     }
     if ("tropical_fish".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.TROPICAL_FISH)
-              // .food(FoodComponents.TROPICAL_FISH, ConsumableComponents.TROPICAL_FISH)
+              .food(CustomFoodComponents.TROPICAL_FISH, CustomConsumableComponents.TROPICAL_FISH)
               .maxCount(16));
     }
     if ("pufferfish".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.PUFFERFISH, ConsumableComponents.PUFFERFISH)
+              .food(CustomFoodComponents.PUFFERFISH, CustomConsumableComponents.PUFFERFISH)
               .maxCount(16));
     }
     if ("cooked_cod".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.COOKED_COD)
-              // .food(FoodComponents.COOKED_COD, ConsumableComponents.COOKED_COD)
+              .food(CustomFoodComponents.COOKED_COD, CustomConsumableComponents.COOKED_COD)
               .maxCount(16));
     }
     if ("cooked_salmon".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.COOKED_SALMON)
-              // .food(FoodComponents.COOKED_SALMON, ConsumableComponents.COOKED_SALMON)
+              .food(CustomFoodComponents.COOKED_SALMON, CustomConsumableComponents.COOKED_SALMON)
               .maxCount(16));
     }
     if ("cookie".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.COOKIE)
-              // .food(FoodComponents.COOKIE, ConsumableComponents.COOKIE)
+              .food(CustomFoodComponents.COOKIE, CustomConsumableComponents.COOKIE)
               .maxCount(16));
     }
     if ("melon_slice".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.MELON_SLICE)
-              // .food(FoodComponents.MELON_SLICE, ConsumableComponents.MELON_SLICE)
+              .food(CustomFoodComponents.MELON_SLICE, CustomConsumableComponents.MELON_SLICE)
               .maxCount(16));
     }
     if ("dried_kelp".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.DRIED_KELP, ConsumableComponents.DRIED_KELP)
+              .food(CustomFoodComponents.DRIED_KELP, CustomConsumableComponents.DRIED_KELP)
               .maxCount(16));
     }
     if ("beef".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.BEEF)
-              // .food(FoodComponents.BEEF, ConsumableComponents.BEEF)
+              .food(CustomFoodComponents.BEEF, CustomConsumableComponents.BEEF)
               .maxCount(16));
     }
     if ("cooked_beef".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.COOKED_BEEF)
-              // .food(FoodComponents.COOKED_BEEF, ConsumableComponents.COOKED_BEEF)
+              .food(CustomFoodComponents.COOKED_BEEF, CustomConsumableComponents.COOKED_BEEF)
               .maxCount(16));
     }
     if ("chicken".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.CHICKEN, ConsumableComponents.RAW_CHICKEN)
+              .food(CustomFoodComponents.CHICKEN, CustomConsumableComponents.CHICKEN)
               .maxCount(16));
     }
     if ("cooked_chicken".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.COOKED_CHICKEN)
-              // .food(FoodComponents.COOKED_CHICKEN, ConsumableComponents.COOKED_CHICKEN)
+              .food(CustomFoodComponents.COOKED_CHICKEN, CustomConsumableComponents.COOKED_CHICKEN)
               .maxCount(16));
     }
     if ("rotten_flesh".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.ROTTEN_FLESH, ConsumableComponents.ROTTEN_FLESH)
+              .food(CustomFoodComponents.ROTTEN_FLESH, CustomConsumableComponents.ROTTEN_FLESH)
               .maxCount(16));
     }
     if ("spider_eye".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.SPIDER_EYE, ConsumableComponents.SPIDER_EYE)
+              .food(CustomFoodComponents.SPIDER_EYE, CustomConsumableComponents.SPIDER_EYE)
               .maxCount(16));
     }
     if ("carrot".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.CARROT)
-              // .food(FoodComponents.CARROT, ConsumableComponents.CARROT)
+              .food(CustomFoodComponents.CARROT, CustomConsumableComponents.CARROT)
               .maxCount(16));
     }
     if ("potato".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.POTATO)
-              // .food(FoodComponents.POTATO, ConsumableComponents.POTATO)
+              .food(CustomFoodComponents.POTATO, CustomConsumableComponents.POTATO)
               .maxCount(16));
     }
     if ("baked_potato".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.BAKED_POTATO)
-              // .food(FoodComponents.BAKED_POTATO, ConsumableComponents.BAKED_POTATO)
+              .food(CustomFoodComponents.BAKED_POTATO, CustomConsumableComponents.BAKED_POTATO)
               .maxCount(16));
     }
     if ("poisonous_potato".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.POISONOUS_POTATO, ConsumableComponents.POISONOUS_POTATO)
+              .food(CustomFoodComponents.POISONOUS_POTATO, CustomConsumableComponents.POISONOUS_POTATO)
               .maxCount(16));
     }
     if ("golden_carrot".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.GOLDEN_CARROT)
-              // .food(FoodComponents.GOLDEN_CARROT, ConsumableComponents.GOLDEN_CARROT)
+              .food(CustomFoodComponents.GOLDEN_CARROT, CustomConsumableComponents.GOLDEN_CARROT)
               .maxCount(16));
     }
     if ("pumpkin_pie".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.PUMPKIN_PIE)
-              // .food(FoodComponents.PUMPKIN_PIE, ConsumableComponents.PUMPKIN_PIE)
+              .food(CustomFoodComponents.PUMPKIN_PIE, CustomConsumableComponents.PUMPKIN_PIE)
               .maxCount(16));
     }
     if ("rabbit".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.RABBIT)
-              // .food(FoodComponents.RABBIT, ConsumableComponents.RABBIT)
+              .food(CustomFoodComponents.RABBIT, CustomConsumableComponents.RABBIT)
               .maxCount(16));
     }
     if ("cooked_rabbit".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.COOKED_RABBIT)
-              // .food(FoodComponents.COOKED_RABBIT, ConsumableComponents.COOKED_RABBIT)
+              .food(CustomFoodComponents.COOKED_RABBIT, CustomConsumableComponents.COOKED_RABBIT)
               .maxCount(16));
     }
     if ("rabbit_stew".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.RABBIT_STEW)
-              // .food(FoodComponents.RABBIT_STEW, ConsumableComponents.RABBIT_STEW)
+              .food(CustomFoodComponents.RABBIT_STEW, CustomConsumableComponents.RABBIT_STEW)
               .useRemainder(Items.BOWL)
               .maxCount(16));
     }
     if ("mutton".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.MUTTON)
-              // .food(FoodComponents.MUTTON, ConsumableComponents.MUTTON)
+              .food(CustomFoodComponents.MUTTON, CustomConsumableComponents.MUTTON)
               .maxCount(16));
     }
     if ("cooked_mutton".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.COOKED_MUTTON)
-              // .food(FoodComponents.COOKED_MUTTON, ConsumableComponents.COOKED_MUTTON)
+              .food(CustomFoodComponents.COOKED_MUTTON, CustomConsumableComponents.COOKED_MUTTON)
               .maxCount(16));
     }
     if ("chorus_fruit".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.CHORUS_FRUIT, ConsumableComponents.CHORUS_FRUIT)
+              .food(CustomFoodComponents.CHORUS_FRUIT, CustomConsumableComponents.CHORUS_FRUIT)
               .useCooldown(1.0F)
               .maxCount(16));
     }
     if ("beetroot".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.BEETROOT)
-              // .food(FoodComponents.BEETROOT, ConsumableComponents.BEETROOT)
+              .food(CustomFoodComponents.BEETROOT, CustomConsumableComponents.BEETROOT)
               .maxCount(16));
     }
     if ("beetroot_soup".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.BEETROOT_SOUP)
-              // .food(FoodComponents.BEETROOT_SOUP, ConsumableComponents.BEETROOT_SOUP)
+              .food(CustomFoodComponents.BEETROOT_SOUP, CustomConsumableComponents.BEETROOT_SOUP)
               .useRemainder(Items.BOWL)
               .maxCount(16));
     }
     if ("sweet_berries".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.SWEET_BERRIES)
-              // .food(FoodComponents.SWEET_BERRIES, ConsumableComponents.SWEET_BERRIES)
-              .maxCount(64));
+              .food(CustomFoodComponents.SWEET_BERRIES, CustomConsumableComponents.SWEET_BERRIES)
+              .maxCount(16));
     }
     if ("glow_berries".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.GLOW_BERRIES)
-              // .food(FoodComponents.GLOW_BERRIES, ConsumableComponents.GLOW_BERRIES)
-              .maxCount(64));
+              .food(CustomFoodComponents.GLOW_BERRIES, CustomConsumableComponents.GLOW_BERRIES)
+              .maxCount(16));
     }
     if ("suspicious_stew".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.SUSPICIOUS_STEW)
-              // .food(FoodComponents.SUSPICIOUS_STEW, ConsumableComponents.SUSPICIOUS_STEW)
+              .food(CustomFoodComponents.SUSPICIOUS_STEW, CustomConsumableComponents.SUSPICIOUS_STEW)
               .component(DataComponentTypes.SUSPICIOUS_STEW_EFFECTS, SuspiciousStewEffectsComponent.DEFAULT)
               .useRemainder(Items.BOWL)
               .maxCount(16));
@@ -265,7 +239,7 @@ public abstract class FoodItemsMixin {
     }
     if ("honey_bottle".equals(id)) {
       return Items.register(id, new Item.Settings()
-              .food(FoodComponents.HONEY_BOTTLE, ConsumableComponents.HONEY_BOTTLE)
+              .food(CustomFoodComponents.HONEY_BOTTLE, ConsumableComponents.HONEY_BOTTLE)
               .recipeRemainder(Items.GLASS_BOTTLE)
               .useRemainder(Items.GLASS_BOTTLE)
               .maxCount(4));
